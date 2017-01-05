620fa66a979b6bd5fde2808a7f5a5f4c58f23fd6
Chris Hill-Scott <me@quis.cc>
2017-01-05 14:14:30 +0000

    Make the url look even more GOV.UK-esq 😈

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,11 @@ var express = require('express')
 var router = express.Router()
 
 // Route index page
-router.get('/', function (req, res) {
+router.get(['/', '/gov.uk', '/gov.uk/track'], function (req, res) {
   res.render('index', {
     'token': req.query.token
   });
-})
+});
 
 // add your routes here
 
